28e17754b2f77778b6d58db9c41854cdc42d4899
pmeaney <meaney.patrick@gmail.com>
2018-11-17 16:54:45 -0800

    got server sent events working

--- src/client/testing_components/EventSource.js
+++ src/client/testing_components/EventSource.js
@@ -5,27 +5,50 @@ import React from 'react'
 */
 class List extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      data: null
+    }
+  }
 
-    const eventSource = new EventSource('http://localhost:3000/eventstream')
-
-    eventSource.addEventListener('message', (data) => {
-      let json = JSON.parse(data)
-      console.log('data from eventsource is', json)
-
-    });
+  componentDidMount() {
+    var es = new EventSource('http://localhost:3000/eventstream')
+    es.onmessage = (e) => {
+      this.setState({ data: e.data })
+    }
   }
 
   render() {
     return (
       <div>
-        Testing
+        {this.state.data ? this.state.data : "nothing to show here"}
       </div>
-      // <ul>
-      //   {this.state.messages.map(m => (<li>{m}</li>))}
-      // </ul>
     )
   }
 }
+// class List extends React.Component {
+//   constructor(props) {
+//     super(props)
+
+//     const eventSource = new EventSource('http://localhost:3000/eventstream')
+
+//     eventSource.addEventListener('message', (data) => {
+//       let json = JSON.parse(data)
+//       console.log('data from eventsource is', json)
+
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         Testing
+//       </div>
+//       // <ul>
+//       //   {this.state.messages.map(m => (<li>{m}</li>))}
+//       // </ul>
+//     )
+//   }
+// }
 
 export default List
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var SSE = require('express-sse')
+var sse = new SSE()
+
 const EventEmitter = require('events');
 
 class MyEmitter extends EventEmitter { }
 const myEmitter = new MyEmitter();
 
+/* --- nomoney4u's example begin --- */
+router.use('/stream', sse.init)
+
+/* --- nomoney4u's example end--- */
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express'});
 });
 
+
 router.get('/eventstream', (req, res, next) => {
+  // Sends out messages as they're emitted.  Is subscribed to.
   res.set({
     'Content-Type': 'text/event-stream',
     'Cache-Control': 'no-cache',
@@ -20,14 +29,21 @@ router.get('/eventstream', (req, res, next) => {
 
   myEmitter.on('message', data => {
     res.write(`event: message\n`);
-    res.write(`${JSON.stringify(data)}\n\n`);
+    res.write(`data: ${JSON.stringify(data)}\n\n`);
     // res.status(200).json(data) ;
   });
 
 });
 
-router.get('/test_time', function (req, res, next) {
 
+router.get('/test_sse', function (req, res, next) {
+  // Just renders a page which monitors /eventstream
+  res.render('testing/test_basicSSE');
+
+});
+
+router.get('/test_time', function (req, res, next) {
+  // Emits a message
   myEmitter.emit('message', {
     title: 'New message!',
     timestamp: new Date()
