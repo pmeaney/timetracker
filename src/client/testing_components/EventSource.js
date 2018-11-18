1adc7d4247b05c551b4a6bcfa544112b51fcb3d7
pmeaney <meaney.patrick@gmail.com>
2018-11-17 16:17:56 -0800

    testing eventsource

--- src/client/testing_components/EventSource.js
+++ src/client/testing_components/EventSource.js
@@ -7,29 +7,15 @@ class List extends React.Component {
   constructor(props) {
     super(props)
 
-    // this.state = {
-    //   source: new EventSource('http://localhost:3000/eventstream'),
-    //   messages: ['hello', 'world']
-    // }
-  }
-  componentDidMount() {
-    // const { source, messages } = this.state
-    // source.addEventListener('message', message => {
-    //   console.log('message is', message)
-    //   this.setState({ messages: messages.concat(message) })
-    // })
-
     const eventSource = new EventSource('http://localhost:3000/eventstream')
 
     eventSource.addEventListener('message', (data) => {
       let json = JSON.parse(data)
       console.log('data from eventsource is', json)
-      // this.state.products.push(json.name)
-      // this.setState({
-      //   products: this.state.products
-      // })
+
     });
   }
+
   render() {
     return (
       <div>
